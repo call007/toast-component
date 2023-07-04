17d85562532458c9cd494cffd573c9b2621bab3a
Ed Shevtsov <e.shevtsov@unitedtraders.com>
2023-07-04 15:11:36 +0200

    repair deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/toast-component/",
+  base: "",
+  publicDir: true,
   plugins: [react()],
   build: {
     emptyOutDir: true,
